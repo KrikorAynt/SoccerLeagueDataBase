952b8ecc262eed9abd8e00aca15af659b2c315c2
arngz <62401562+arngz@users.noreply.github.com>
2020-11-17 17:24:01 -0500

    fix compile errors, implement output panel also cleaned up buildButtonPanel, fixed up the panel layout, and implemented an exit button

--- GuiPrototype.java
+++ GuiPrototype.java
@@ -1,66 +1,81 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class GuiPrototype {
-    private JPanel buttonPanel, inputPanel;
+    //these are instance variables so they are accessible by the actionlisteners
+    private JFrame frame;
     private JTextArea output;
     
     //reads a whole file and return the contents as a string
     private static String getFileContents(String file) {
         String content = "";
         try {
-            text = new String(Files.readAllBytes(Paths.get(file)));
+            content = new String(Files.readAllBytes(Paths.get(file)));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return content;
     }
     
-    private void buildButtonPanel() {
-        JButton createTablesBtn = new JButton("Create tables");
-        JButton initTablesBtn = new JButton("Populate tables");
-        JButton dropTablesBtn = new JButton("Drop tables");
-        JButton advQueriesBtn = new JButton("Run advanced queries");
-        JButton simpQueriesBtn = new JButton("Run simple queries");
-        
-        QuerySender qs = new QuerySender();
-        createTablesBtn.addActionListener(qs);
-        initTablesBtn.addActionListener(qs);
-        dropTablesBtn.addActionListener(qs);
-        advQueriesBtn.addActionListener(qs);
-        simpQueriesBtn.addActionListener(qs);
-        
-        //qs will read the queries from the filenames given here
-        //and send them to the db
-        createTablesBtn.setActionCommand("create_tables.txt");
-        initTablesBtn.setActionCommand("populate_tables.txt");
-        dropTablesBtn.setActionCommand("drop_tables.txt");
-        advQueriesBtn.setActionCommand("adv_queries.txt");
-        simpQueriesBtn.setActionCommand("simp_queries.txt");
-
-        buttonPanel.add(createTablesBtn);
-        buttonPanel.add(initTablesBtn);
-        buttonPanel.add(dropTablesBtn);
-        buttonPanel.add(advQueriesBtn);
-        buttonPanel.add(simpQueriesBtn);
+    private JButton createButton(String text, String actioncmd, ActionListener al) {
+        JButton newButton = new JButton(text);
+        newButton.addActionListener(al);
+        newButton.setActionCommand(actioncmd);
+        return newButton;
     }
 
-    /*public void buildInputPanel() {
+    private JPanel buildButtonPanel() {
+        JPanel buttonPanel = new JPanel(new BorderLayout());
+        JPanel top = new JPanel(new GridLayout(0,1));
+
+        QuerySender qs = new QuerySender();
+        top.add(createButton("Create tables", "create_tables.txt", qs));
+        top.add(createButton("Populate tables", "populate_tables.txt", qs));
+        top.add(createButton("Drop tables", "drop_tables.txt", qs));
+        top.add(createButton("Run advanced queries", "adv_queries.txt", qs));
+        top.add(createButton("Run simple queries", "simp_queries.txt", qs));
+
+        buttonPanel.add(top, BorderLayout.PAGE_START);
+        JButton exitBtn = new JButton("Exit");
+        exitBtn.addActionListener(new ExitListener());
+        buttonPanel.add(exitBtn, BorderLayout.PAGE_END);
+
+        return buttonPanel;
+    }
+
+    private JPanel buildOutputPanel() {
+        JPanel outPanel = new JPanel(new BorderLayout());
+
+        output = new JTextArea(20,40);
+        output.setEditable(false);
+        JScrollPane sp = new JScrollPane(output);
+
+        outPanel.add(new JLabel("Query output"), BorderLayout.PAGE_START);
+        outPanel.add(sp, BorderLayout.CENTER);
+
+        return outPanel;
+    }
+
+    /*private JPanel buildInputPanel() {
         //this panel will have fields for inputting data into a table
         //it will be a cardlayout to switch "scenes" depending on the table
         //either a dropdown or multiple buttons to select which table
     }*/
 
     public GuiPrototype() {
-        JFrame frame = new JFrame("Soccer League DBMS");
+        frame = new JFrame("Soccer League DBMS");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(600, 400);
-
-        buildButtonPanel();
-        frame.add(buttonPanel);
+        
+        JPanel buttonPanel = buildButtonPanel();
+        frame.add(buttonPanel, BorderLayout.LINE_START);
+        
+        JPanel outPanel = buildOutputPanel();
+        frame.add(outPanel, BorderLayout.CENTER);
 
         frame.pack();
         frame.setVisible(true);
@@ -75,12 +90,26 @@ public class GuiPrototype {
         }
     }*/
 
+    private class ExitListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            GuiPrototype.this.frame.dispose();
+            //TODO also disconnect from db
+        }
+    }
+
     private class QuerySender implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             //read queries from file specified by the button's actioncommand
-            String queries = GuiPrototype.readFileContents(e.getActionCommand());
-            //send the queries string to the db
-            //output the result into the JTextArea output
+            String queries = GuiPrototype.getFileContents(e.getActionCommand());
+            /* TODO: 
+             * send the queries string to the db
+             * output the result into the JTextArea output
+             */
+            System.out.println(queries); //just for testing
         }
+    }
+
+    public static void main(String[] args) {
+        GuiPrototype gp = new GuiPrototype();
     }
 }
